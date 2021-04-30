a4d1ec1873bd64e544c7f88954890d18cfde5228
Hans Pelle Jart <hanspellejart@gmail.com>
2021-04-30 13:15:37 +0200

    updatede api

--- website/app.js
+++ website/app.js
@@ -1,10 +1,47 @@
 // Global Variables 
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
- 
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 // API key
-let apiKey = '&appid=5fc069c6cbb6add77842ae6227652287';
+let apiKey = '&units=metric&appid=5fc069c6cbb6add77842ae6227652287';
+ 
+document.getElementById('generate').addEventListener('click', performAction)
+
+//Function to get web api data
+const getWeather = async (baseURL, zip, key) => {
+    
+    const response = await fetch (baseURL+zip+key)
+    try{
+       const data = await response.json();
+       return data;
+    }catch(error){
+       console.log("error", error);
+    }
+  }
+
+
+  // Post
+ const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+    try {
+    const newData = await response.json();
+    return newData
+    }catch(error) {
+    console.log("error", error);
+  }
+}
+
+
+
 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
