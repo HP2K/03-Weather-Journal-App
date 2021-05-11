cee4ad342031b665ae0be16fd2f91d60f9516f1a
Hans Pelle Jart <hanspellejart@gmail.com>
2021-05-11 09:24:07 +0200

    api key update

--- website/app.js
+++ website/app.js
@@ -1,11 +1,11 @@
 // Global Variables 
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 // API key
-let apiKey = '&units=metric&appid=5fc069c6cbb6add77842ae6227652287';
+const apiKey = '&appid=5fc069c6cbb6add77842ae6227652287&units=imperial';
  
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let day = new Date();
+let newDate = (day.getMonth() + 1) + '/'+ day.getDate()+'/'+ day.getFullYear();
 
 // Event listener on generate button
 document.getElementById('generate').addEventListener('click', performAction);
